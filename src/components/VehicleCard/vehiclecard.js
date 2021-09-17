648d3843b95c9c09e1ecab82827e1e3bed4996a2
lexican <miamilexican@gmail.com>
2021-09-17 16:03:46 +0100

    feat: added vehicle data

--- src/components/VehicleCard/vehiclecard.js
+++ src/components/VehicleCard/vehiclecard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Car from "../../assets/images/car.jpg"
 import './vehiclecard.scss'
-export default function Vehiclecard() {
+export default function Vehiclecard(props) {
   return (
     <div className="card">
-      <img src={Car}></img>
+      <img src={props.item.images[0]}></img>
       <div className="inner-text-container">
-        <p>Car</p>
+        <p>{props.item.name}</p>
       </div>
     </div>
   );
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/navbar";
 import "./home.scss";
 import Search from "../../assets/icons/search.svg";
 import Vehiclecard from "../../components/VehicleCard/vehiclecard";
-export default function home() {
+import { data } from "../../data/data";
+import { useHistory } from "react-router-dom";
+
+export default function Home() {
+  let history = useHistory();
+  const [text, setText] = useState("");
+
+  const handleSearch = () => {
+    if (text.length > 0) {
+      history.push(`/result/${text}`);
+    }
+  };
+
   return (
     <div className="home">
       <div className="banner">
@@ -11,24 +23,21 @@ export default function home() {
         <div className="inner-container">
           <h2>Vehicle encyclopedia</h2>
           <div className="input-container">
-            <img src={Search} />
+            <img src={Search} onClick={handleSearch} />
             <input
               type="text"
               placeholder="Seach any type of vehicle"
               className="form-control"
+              onChange={(e) => setText(e.target.value)}
             ></input>
           </div>
         </div>
         <section className="main">
           <h4>Select a type of vehicle</h4>
           <div className="vehicles">
-            <Vehiclecard />
-            <Vehiclecard />
-            <Vehiclecard />
-            <Vehiclecard />
-            <Vehiclecard />
-            <Vehiclecard />
-            <Vehiclecard />
+            {data.map((item, index) => {
+              return <Vehiclecard item={item} />;
+            })}
           </div>
         </section>
       </div>
