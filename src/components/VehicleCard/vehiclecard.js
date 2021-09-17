54b694071de78069c6f6d4728b62ea4776017eac
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-09-17 17:32:20 +0100

    feat: route to result page

--- src/components/VehicleCard/vehiclecard.js
+++ src/components/VehicleCard/vehiclecard.js
@@ -1,12 +1,21 @@
-import React from "react";
-import './vehiclecard.scss'
-export default function Vehiclecard(props) {
-  return (
-    <div className="card">
-      <img src={props.item.images[0]} alt={props.item.name}></img>
-      <div className="inner-text-container">
-        <p>{props.item.name}</p>
-      </div>
-    </div>
-  );
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './vehiclecard.scss';
+
+export default function Vehiclecard({ item }) {
+	const history = useHistory();
+
+	return (
+		<div
+			className='card'
+			onClick={() =>
+				history.push({ pathname: `/result/${item.name}`, state: { item } })
+			}
+		>
+			<img src={item.images[0]} alt={item.name}></img>
+			<div className='inner-text-container'>
+				<p>{item.name}</p>
+			</div>
+		</div>
+	);
 }
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import Navbar from "../../components/Navbar/navbar";
-import "./home.scss";
-import Search from "../../assets/icons/search.svg";
-import Vehiclecard from "../../components/VehicleCard/vehiclecard";
-import { data } from "../../data/data";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Search from '../../assets/icons/search.svg';
+import Navbar from '../../components/Navbar/navbar';
+import Vehiclecard from '../../components/VehicleCard/vehiclecard';
+import { data } from '../../data';
+import './home.scss';
 
 export default function Home() {
-  let history = useHistory();
-  const [text, setText] = useState("");
+	let history = useHistory();
+	const [text, setText] = useState('');
 
-  const handleSearch = () => {
-    if (text.length > 0) {
-      history.push(`/result/${text}`);
-    }
-  };
+	const handleSearch = () => {
+		if (text.length > 0) {
+			history.push(`/result/${text}`);
+		}
+	};
 
-  return (
-    <div className="home">
-      <div className="banner">
-        <Navbar />
-        <div className="inner-container">
-          <h2>Vehicle encyclopedia</h2>
-          <div className="input-container">
-            <img src={Search} onClick={handleSearch} alt="Search icon" />
-            <input
-              type="text"
-              placeholder="Seach any type of vehicle"
-              className="form-control"
-              onChange={(e) => setText(e.target.value)}
-            ></input>
-          </div>
-        </div>
-        <section className="main">
-          <h4>Select a type of vehicle</h4>
-          <div className="vehicles">
-            {data.map((item, index) => {
-              return <Vehiclecard item={item} />;
-            })}
-          </div>
-        </section>
-      </div>
-    </div>
-  );
+	return (
+		<div className='home'>
+			<div className='banner'>
+				<Navbar />
+				<div className='inner-container'>
+					<h2>Vehicle encyclopedia</h2>
+					<div className='input-container'>
+						<img src={Search} onClick={handleSearch} alt='Search icon' />
+						<input
+							type='text'
+							placeholder='Seach any type of vehicle'
+							className='form-control'
+							onChange={(e) => setText(e.target.value)}
+						></input>
+					</div>
+				</div>
+				<section className='main'>
+					<h4>Select a type of vehicle</h4>
+					<div className='vehicles'>
+						{data.map((item) => {
+							return <Vehiclecard item={item} />;
+						})}
+					</div>
+				</section>
+			</div>
+		</div>
+	);
 }
