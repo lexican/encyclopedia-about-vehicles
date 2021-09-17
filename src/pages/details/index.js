f2d34f3b96b67b9135eda583f4335f98d2d5e26e
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-09-17 20:33:46 +0100

    feat: next, prev func

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,26 +1,40 @@
-import { useLocation } from 'react-router';
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import ItemDetails from '../../components/ItemDetails';
 import Navbar from '../../components/Navbar/navbar';
 import './index.scss';
 
 export default function DetailsPage() {
+	const [currentImage, setCurrentImage] = useState(0);
 	const location = useLocation();
-
 	const item = location.state?.item;
-	console.log('item', item);
+
+	const previous = () => {
+		setCurrentImage((prev) => {
+			if (prev > 0) return prev - 1;
+			return prev;
+		});
+	};
+
+	const next = () => {
+		setCurrentImage((prev) => {
+			if (prev < item.images.length - 1) return prev + 1;
+			return prev;
+		});
+	};
 
 	return (
 		<div
 			className='details-page'
 			style={{
-				backgroundImage: `url(${item.images[0]})`,
+				backgroundImage: `url(${item.images[currentImage]})`,
 			}}
 		>
 			<div className='nav-wrapper'>
 				<Navbar />
 			</div>
 
-			<div className='left-nav'>
+			<div className='left-nav' onClick={previous}>
 				<img src={require('../../assets/icons/left.svg').default} alt='' />
 			</div>
 
@@ -28,7 +42,7 @@ export default function DetailsPage() {
 				<ItemDetails item={item} />
 			</div>
 
-			<div className='right-nav'>
+			<div className='right-nav' onClick={next}>
 				<img src={require('../../assets/icons/right.svg').default} alt='' />
 			</div>
 		</div>
